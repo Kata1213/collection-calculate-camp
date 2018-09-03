8d8f81c6864669eb6cfc01f48bc21435bee66bfd
Jingyue Li <jingyli@CNjingyli.local>
2018-09-03 21:42:52 +0800

    [Jingyue] finish collect_all_even and pass the test

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,12 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var result =collection.filter(function (x) {
+    // console.log(x%2===0);
+    return x % 2 ===0;
+  });
+  // console.log(result);
+return result;
 }
 
 module.exports = collect_all_even;
