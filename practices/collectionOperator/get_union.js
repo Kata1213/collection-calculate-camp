c51dd49feed45ec24a546e53dd8739d0adde7019
Jingyue Li <jingyli@CNjingyli.local>
2018-09-03 23:51:29 +0800

    [Jingyue] finish get_union and pass the test

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,9 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  let union = Array.from(new Set(collection_a.concat(collection_b)));
+
+  return union;
 }
 
 module.exports = get_union;
