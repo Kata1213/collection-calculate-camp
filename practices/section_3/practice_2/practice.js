975e184f2a443ba4e7dd21c51cb5b6c485ceb0f3
Jingyue Li <jingyli@thoughtworks.com>
2018-09-04 21:32:19 +0800

    [Jingyue]finish practice-3-2 and pass the tests

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,13 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var collection_b = object_b["value"];
+  for(let i=0;i<collection_a.length;i++){
+    var k = collection_a[i].key;
+    if(collection_b.indexOf(k)!=-1){
+      collection_a[i].count -= Math.floor((collection_a[i].count)/3);
+    }
+  }
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
