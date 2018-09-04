b9731c864a9fecf2267924fc79a3298c8af91e24
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 08:36:18 +0800

    [Jingyue] finish choose_even and pass the test

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,10 @@
 function choose_even(collection) {
 
   //在这里写入代码
+  var result = collection.filter(function (x) {
+      return x % 2===0;
+  })
+  return result;
 }
 
 module.exports = choose_even;
