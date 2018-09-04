2b720c564ebeee4e4a9800edc848a4ae28905d8d
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 08:47:59 +0800

    [JIngyue]finish grouping_count and pass the test

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,8 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  //https://blog.csdn.net/fighting__hg/article/details/52044594
+  return collection.reduce((p,k) => (p[k]++ || (p[k] = 1),p),{});
 }
 
 module.exports = grouping_count;
