e9bf36d719eea95e4cd8dd8440d3cde48e070e01
Jingyue Li <jingyli@thoughtworks.com>
2018-09-04 14:55:37 +0800

    [Jingyue] finish compute_average_spec.js and pass test

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -11,3 +11,47 @@ function compute_average(collection) {
 
 module.exports = compute_average;
 
+
+
+
+
+//课上练习，错误答案⤵ ，
+// function mainn(template , obj){
+//   return template(obj);
+// }
+// var obj = {
+//   firstName:"CHENG",
+//   lastName:"Li"
+// }
+//
+// function template(){
+//   var result=[];
+//   result.push(" I am ");
+//   result.push(obj.firstName);
+//   result.push (" , ");
+//   result.push(obj.lastName);
+//   return result;
+// }
+//
+// console.log(mainn(template,obj));
+
+
+
+
+
+
+//正确答案~~！！！！
+// var s ="I am ${firstName} , ${lastName}";
+// var object= {
+//   firstName:"Chenge",
+//     lastName:"Li"
+// };
+//
+// function repalce(strings ,object) {
+//   console.log(Object.keys(object).reduce(reducer,strings));
+// }
+//
+// const reducer=(s1,s2)=>s1.replace('${'+s2+'}',object[s2]);
+//
+// repalce(s,object);
+
