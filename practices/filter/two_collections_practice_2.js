e8aec021a7966926e40127196f16570bc072603a
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 10:00:05 +0800

    [Jingyue]finish choose_no_common_elements and pass the test

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,6 +3,10 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var result =collection_a.filter(function (x) {
+    return collection_b.indexOf(x) ===-1;
+  })
+  return result;
 }
 
 module.exports = choose_no_common_elements;
