a804a137ec7e08ca8770e4c387614729c5938280
Jingyue Li <jingyli@thoughtworks.com>
2018-09-04 23:11:58 +0800

    [Jingyue] finish interval_operation pakege

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -3,6 +3,16 @@
 function average_uneven(collection) {
 
   //在这里写入代码
+  return get_avg(collection.filter(function (x) {
+    return x %2 !==0;
+  }))
+}
+
+function get_avg(collection){
+ var sum = collection.reduce(function (pre,cur) {
+    return pre+cur;
+  })
+  return sum/collection.length;
 }
 
 module.exports = average_uneven;
--- practices/superposition_operation/interval_operation/median_to_letter.js
+++ practices/superposition_operation/interval_operation/median_to_letter.js
@@ -3,6 +3,30 @@
 function median_to_letter(collection) {
 
   //在这里写入代码
+  var len = collection.length;
+  let median;
+
+  if(len % 2 == 0 ){
+    median = (collection[len/2-1]+collection[len/2])/2;
+    median = Math.ceil(median);
+  }else{
+    let mediumIndex = Math.floor(len/2);
+    median = collection[mediumIndex];
+  }
+
+  return numToCharacter(median);
+}
+
+function numToCharacter(num) {
+  var ele;
+  if (num <= 26) {
+    ele = String.fromCharCode(96 + num);
+  } else {
+    let num1 = Math.floor((num - 1) / 26),
+      num2 = num - num1 * 26;
+    ele = String.fromCharCode(96 + num1) + String.fromCharCode(96 + num2);
+  }
+  return ele;
 }
 
 module.exports = median_to_letter;
