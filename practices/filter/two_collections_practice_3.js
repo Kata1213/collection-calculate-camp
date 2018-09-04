4c83a5ec91c8591473bcdb6bba9b18382316d1b1
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 10:16:43 +0800

    [Jingyue] finish choose_divisible_integer and pass the test

--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,26 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  var result = collection_a.filter(function (x) {
+    // collection_b.filter(function (y) {
+    //    if(x % y ===0){
+    //      console.log(x + "   " + y);
+    //      return true;
+    //    }
+    //   return false;
+    // })
+
+    for (let i = 0; i < collection_b.length; i++) {
+      if (x % collection_b[i] === 0) {
+        console.log(x + "   " + collection_b[i]);
+        return true;
+      }
+
+    }
+    return false;
+  })
+
+  return result;
 }
 
 module.exports = choose_divisible_integer;
