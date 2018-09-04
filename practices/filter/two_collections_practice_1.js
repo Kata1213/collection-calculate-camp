23c6de5bff04f4af93df2a282a4d77c847ff6686
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 09:03:22 +0800

    [Jingyue] finish function choose_common_elements(collection_a, collection_b) {

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -3,6 +3,23 @@
 function choose_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+
+  // var collection =collection_a.concat(collection_b);
+  // console.log(collection);
+
+  // var result = collection.filter(function (element, index, self) {
+  //   return self.indexOf(element) !== index;
+  // });
+  // return result;
+
+
+  var result = collection_a.filter(function(x){
+    // return collection_a.contains(x);  不能用这样的方法去判断是否包含！！
+    return collection_b.indexOf(x) > -1;   //得用indexOf的方法去判断！！！！！！！！！！！
+  })
+  console.log(result);
+  return result;
+
 }
 
 module.exports = choose_common_elements;
