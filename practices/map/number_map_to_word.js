5928138fc0042f4d317ae8ca5a42c2cd0a8e2927
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 10:50:43 +0800

    [Jingyue] pass  number_map_to_word_spec.js

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,15 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  // return ['a','b','c','d','e'];
+  // String.fromCharCode(96 + number_a)
+
+  var result = [];
+  for(var x of collection){
+    result.push(String.fromCharCode(96 + x));
+  }
+  return result;
+
+
 };
 
 module.exports = number_map_to_word;
