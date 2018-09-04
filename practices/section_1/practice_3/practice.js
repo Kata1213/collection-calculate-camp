89c19a613c4b8bc02b31e80dd4aecb3a30f2330b
Jingyue Li <jingyli@thoughtworks.com>
2018-09-04 16:56:39 +0800

    [Jingyue] finish practices/section_1/practice_3/practice.js and pass the test

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,13 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var bb=object_b.value;
+  return find(collection_a, bb);
+}
+
+function find(collection_a,collection_b) {
+  return collection_a.filter(function (x) {
+    return collection_b.indexOf(x) > -1;
+  })
 }
 
 module.exports = collect_same_elements;
