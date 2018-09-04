44844c141b41cfa9adefa15c5a016317e9658439
Jingyue Li <jingyli@thoughtworks.com>
2018-09-04 22:03:02 +0800

    [Jingyue]finish practice-3-4 and pass the test

--- practices/section_3/practice_4/practice.js
+++ practices/section_3/practice_4/practice.js
@@ -1,5 +1,37 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var countSame = count_same_elements(collection_a);
+  var collection_b = object_b["value"];
+  for(let i=0;i<countSame.length;i++){
+    var k = countSame[i].key;
+    if(collection_b.indexOf(k)!=-1){
+      countSame[i].count -= Math.floor((countSame[i].count)/3);
+    }
+  }
+  return countSame;
+
+}
+
+function count_same_elements(collection) {
+  var obj = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (obj[collection[i]] == undefined) {
+      if (collection[i].indexOf("-") == -1) {
+        obj[collection[i]] = 1;
+      } else {
+        obj[collection[i].split("-")[0]] = +collection[i].split("-")[1];
+      }
+    } else {
+      obj[collection[i]]++;
+    }
+  }
+
+  let result = [];
+  for(var key in obj){
+    var temp = {key:key,count:obj[key]}
+    result.push(temp);
+  }
+  return result;
 }
 
 module.exports = create_updated_collection;
