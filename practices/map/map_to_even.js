23f231302f286b7232fabec2112329985e8715b0
Jingyue Li <jingyli@CNjingyli.local>
2018-09-04 10:42:58 +0800

    [Jingyue] finish map_to_even and pass the test

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,10 @@
 'use strict';
 function map_to_even(collection){
-  return [2,4,6,8,10];
+  // return [2,4,6,8,10];
+  var result = [];
+  for(var x of collection){
+    result.push(x*2);
+  }
+  return result;
 }
 module.exports = map_to_even;
