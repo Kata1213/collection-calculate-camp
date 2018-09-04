30e0cde2bdab33d185a60d885528e30b62fb4300
Jingyue Li <jingyli@thoughtworks.com>
2018-09-04 17:33:28 +0800

    [Jingyue]finish practice-2-1 and pass test

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,21 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var obj = [];
+  for (let i = 0; i < collection.length; i++) {
+    if(obj[collection[i]] == undefined){
+      obj[collection[i]]=1;
+    }else{
+      obj[collection[i]]++;
+    }
+  }
+
+  let result = [];
+  for(var key in obj){
+    var temp = {key:key,count:obj[key]}
+    result.push(temp);
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
+
